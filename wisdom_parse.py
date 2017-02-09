6cadc16a2ca9539ac3c10e40f0b37d8ec50ed0b6
root <root@jwisbell>
2017-02-08 18:38:39 -0600

    Populating the repo

---
+++ wisdom_parse.py
@@ -0,0 +1,38 @@
+import string
+
+def load_wisdom():
+    f = open('forward_plan.txt','r')
+    plan = ''
+    for l in f.readlines():
+        plan = plan+l
+    return plan
+
+
+def read_wisdom():
+    f = open('forward_plan_0.txt')
+    lines = f.readlines()
+    part1 = '' 
+    for l in lines:
+        part1 = part1+l
+    f.close()
+    
+    f = open('forward_plan_1.txt')
+    lines = f.readlines()
+    part2 ='' 
+    for l in lines:
+        part2 = part2+l
+    f.close()
+   
+    f = open('forward_plan_2.txt')
+    lines = f.readlines()
+    part3 = '' 
+    for l in lines:
+        part3 = part3+l
+    f.close()
+
+    return [part1,part2,part3]
+#print read_wisdom()
+#print part1
+#print part2
+#print part3
+
