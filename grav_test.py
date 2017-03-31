4737aae1f844875d9069540cbc713737761dde85
sandbox_computer <sophie-deam@uiowa.edu>
2017-03-31 15:59:54 -0500

    SCRIPT TO AUTOMATICALLY CLICK THE DAMN BUTTON

--- grav_test.py
+++ grav_test.py
@@ -4,7 +4,7 @@ import sys
 from subprocess import call
 import numpy as np
 from astropy.io import fits
-
+import matplotlib.pyplot as plt
 
 #------- Import our files ---------
 import gravity_algorithm2
@@ -21,7 +21,7 @@ ADB_FILEPATH = ''
 NORMALIZATION = 1.
 REFRESH_RATE = 1 #hz
 SLEEP_TIME = 10 #seconds
-ITER = 100000 #number of iteration to orbit over
+ITER = 100 #number of iteration to orbit over
 
 def differ(arr1, arr2):
 	for k in range(len(arr1)):
@@ -37,29 +37,43 @@ if __name__ == '__main__':
 	current_pos = [250.,350.]
 	current_vel = [0.,0.]
 	exit = 0
+	#call('/home/gravity/src/SARndbox-2.2/bin/SARndbox -uhm -fpv -rer 20 100 &', shell=True)
 	while exit == 0:
 		start =time.time()
 		"""
 		REFRESH THE DEM FILE SAVED ON DISK
 		"""
-		call('bash dem_saver_2.sh', shell=True)
+		
+		call('wmctrl -a SARndbox', shell=True)
+		call('xdotool key "B"', shell=True)
+		
 		"""
 		READ IN THE DEM FILE AS NUMPY ARRAY
 		"""
+		
 		dem_file = gdal.Open('/home/gravity/src/SARndbox-2.2/BathymetrySaverTool.dem')
 		# Converts dem_file to numpy array of shape (480, 639)
 		dem_array = np.array(dem_file.GetRasterBand(1).ReadAsArray())
 		# This is a temporary fix for an uncalibrated surface, i.e. we set a base level where any value is less than 3
-		for x in np.nditer(dem_array, op_flags=['readwrite']):
+		'''for x in np.nditer(dem_array, op_flags=['readwrite']):
 		    if x[...] < 3.:
-		        x[...] = 0.
+		        x[...] = 0.'''
+		fig = plt.figure()
+		plt.imshow(dem_array)
+		plt.savefig('/home/gravity/Desktop/color_field.jpg')
+		call('adb shell rm /storage/emulated/0/field.jpg', shell=True)
+		call('adb push /home/gravity/Desktop/color_field.jpg /storage/emulated/0/field.jpg', shell=True)
 		"""
 		CONVOLVE THE DEM-DENSITY FIELD WITH THE PLUMMER KERNEL
 		"""
 		shp = dem_array.shape
+		convstart = time.time()
 		potential_field = np.reshape(convolution.convolve(dem_array, PLUMMER, 'kernel'),shp)
+		
+		
 		potential_field = potential_field/np.max(potential_field)*10
-
+		convend = time.time()
+		print 'convolution took', convend-convstart
 		'''fig = plt.figure()
 		plt.imshow(potential_field)
 		plt.show()
@@ -69,7 +83,7 @@ if __name__ == '__main__':
 
 		READ IN INPUT PARAMS AND UPDATE PARTICLE IF NEEDED
 		"""
-		input_pos, input_vel = io_funcs.read_from_app()
+		#input_pos, input_vel = io_funcs.read_from_app()
 
 		particle = gravity_algorithm2.Particle(current_pos, current_vel, potential_field)
 		'''if differ(input_pos, previous_pos) or differ(input_vel, previous_vel):
@@ -78,17 +92,21 @@ if __name__ == '__main__':
 		"""
 		INTEGRATE FOR A WHILE
 		"""
+
 		to_send = gravity_algorithm2.run_orbit(particle, ITER, edge_mode='reflect') #run for 1000 iterations and save the array
+
 		current_pos = [particle.pos[0], particle.pos[1]] 
 		current_vel = [particle.vel[0], particle.vel[1]]
 		print current_pos, current_vel
 
-		io_funcs.plot_orbit(to_send, potential_field)
+		#io_funcs.plot_orbit(to_send, potential_field)
 		"""
 		SEND IMAGE, ORBIT DATA TO APP
 		"""
-		if not io_funcs.write_to_tablet(to_send):
-			exit = 1
+
+		#if not io_funcs.write_to_tablet(to_send):
+		#exit = 1
+
 		"""
 		CLEAN UP FOR MEMORY MANAGEMENT
 		"""
